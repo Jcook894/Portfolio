946b21ba107d0c84f646fceeb9b59aa31b573b22
Jcook894 <Jcook894@icloud.com>
2017-01-19 02:43:11 -0500

    feat: fixed webpack server and react.

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,29 @@
-var webpack = require('webpack');
-var path = require('path');
+var config = {
+   entry: './main.js',
 
-module.exports = {
-  devtool: "inline-source-map",
-  entry: [
-    'webpack-dev-server/client?http://localhost:8080/',
-    'webpack/hot/only-dev-server',
-    './src'
-  ],
+   output: {
+      path:'./',
+      filename: 'index.js',
+   },
 
-  output: {
-    path: path.join(__dirname,'public'),
-    filename: 'bundle.js'
+   devServer: {
+      inline: true,
+      port: 8080
+   },
 
-  },
-  resolve : {
-    moduleDirectories: ['node_modules', 'src'],
-    extensions: ['','.js']
-  },
+   module: {
+      loaders: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel',
 
-  modules:{
-    loaders:[
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: ['react-hot','babel?preset[]=react,presets[]=es2015']
-      }
-    ]
-  },
-  plugins: [
-     new webpack.HotModuleReplacementPlugin(),
-     new webpack.NoErrorsPlugin()
-  ]
-};
+            query: {
+               presets: ['es2015', 'react']
+            }
+         }
+      ]
+   }
+}
+
+module.exports = config;
