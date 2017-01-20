3b7a1e9ab593141b88ee98eff933b19d27ddf035
Jcook894 <Jcook894@icloud.com>
2017-01-20 01:56:42 -0500

    Feat: Added google fonts and made site more responsive.

--- App.jsx
+++ App.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+var React = require('react');
 
-class App extends React.Component {
-   render() {
-      return (
-         <div>
-            Hello World!!!
-         </div>
+var Skills = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <h1 className="headers" id="About">About Me</h1>
+        <img className="img-circle" src="../assets/pic.JPG"></img>
+
+        <p> Hey everyone! My name is Julian and im a junior Front-End Dev with dreams of becoming a Full-stack Developer! As well as making web sites, I love to playe video games and skateboard in my spare time.</p>
+
+      </div>
       );
-   }
-}
+  }
+}) ;
 
-export default App;
+
+module.exports = Skills;
---
+++ ContactMe.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+var Portfolio = React.createClass({
+    getDefaultProps: function(){
+      return {
+        image: [],
+        interval: 2000
+      }
+    }
+
+
+})
