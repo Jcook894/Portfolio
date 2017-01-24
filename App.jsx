e1c998531b2cabac16f37ef9e949ab76719ee1c4
PaulMusso <jcook894@icloud.com>
2017-01-24 15:38:39 -0500

    feat: removed the carousel and added portfolio grid

--- App.jsx
+++ App.jsx
@@ -1,14 +1,1 @@
-var React = require('react');
-var Carousel = require('./Portfolio.jsx');
 
-var Skills = React.createClass({
-  render: function(){
-    return (
-      
-
-      );
-  }
-}) ;
-
-
-module.exports = Skills;
--- main.js
+++ main.js
@@ -1,12 +1,12 @@
+import { Line, Circle } from 'rc-progress';
+
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Carousel = require('./Portfolio.jsx');
-//var AboutMe = require('./app.jsx');
 
 
 
 
-ReactDOM.render(
-  <AboutMe />,
-  document.getElementById('About')
-);
+ReactDOM.render(<div>
+  <Line percent="10" strokeWidth="4" strokeColor="#D3D3D3" />
+  <Circle percent="10" strokeWidth="4" strokeColor="#D3D3D3" />
+</div>, getElementById('Skills'));
