453c88cebf90b374499a9f91a13c8f3eaff51b2c
PaulMusso <jcook894@icloud.com>
2017-01-25 16:12:10 -0500

    Feat: Added animations to skills.

--- js/port.js
+++ js/port.js
@@ -5,30 +5,88 @@ $(document).ready(function() {
 });
 
 
-
 //Scroll animations.
 $(window).scroll(function() {
 		$('.animateUp').each(function(){
 		var imagePos = $(this).offset().top;
 
 		var topOfWindow = $(window).scrollTop();
-			if (imagePos < topOfWindow+700) {
+			if (imagePos < topOfWindow+710) {
 				$(this).addClass("fadeIn");
 			}
 		});
 });
 
     // Scroll animations for skills, will stretch left and right.
-    $(window).scroll(function(){
-          $('#html').each(function(){
+  $(window).scroll(function(){
+      $('#html').each(function(){
           var imagePos = $(this).offset().top;
 
           var topOfWindow = $(window).scrollTop();
-            if (imagePos < topOfWindow+550) {
-              $(this).addClass("fadeIn");
+            if (imagePos < topOfWindow+700) {
+              $(this).addClass("stretchRight");
             }
+});
+
+      $('#css').each(function(){
+            var imagePos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+              if (imagePos < topOfWindow+700) {
+                $(this).addClass("stretchLeft");
+              }
 
     });
+
+    $('#javascript').each(function(){
+          var imagePos = $(this).offset().top;
+
+          var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow+700) {
+              $(this).addClass("stretchRight");
+            }
+
+  });
+
+  $('#jasmine').each(function(){
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+          if (imagePos < topOfWindow+700) {
+            $(this).addClass("stretchLeft");
+          }
+
+});
+
+$('#knockout').each(function(){
+      var imagePos = $(this).offset().top;
+
+      var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow+700) {
+          $(this).addClass("stretchRight");
+        }
+
+});
+
+$('#webOpt').each(function(){
+      var imagePos = $(this).offset().top;
+
+      var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow+700) {
+          $(this).addClass("stretchLeft");
+        }
+
+});
+
+$('#react').each(function(){
+      var imagePos = $(this).offset().top;
+
+      var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow+700) {
+          $(this).addClass("stretchRight");
+        }
+
+});
 
 
 });
